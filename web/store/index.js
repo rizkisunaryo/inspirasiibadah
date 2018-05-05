0e6d159a0ac85f20905722b736c8eb4102ebee8c
Rizki Sunaryo <rizkisunaryo@gmail.com>
2018-05-05 20:06:09 +0700

    add logger middleware

--- web/store/index.js
+++ web/store/index.js
@@ -2,8 +2,22 @@ import Store, { thunk } from 'repatch'
 
 import api from '../api'
 import testRedux from '../store/testRedux'
+import Development from '../configs/Development';
+import ObjectUtils from '../../utils/ObjectUtils';
 
-export default new Store({
+const store = new Store({
   testRedux
-})
-  .addMiddleware(thunk.withExtraArgument(api))
+}).addMiddleware(thunk.withExtraArgument(api))
+
+if (Development.isDev) {
+  // logger middleware
+  store.addMiddleware(store => next => reducer => {
+    const state = store.getState()
+    const nextState = reducer(state)
+    const diff = ObjectUtils.difference(nextState, state)
+    console.log('\nprevState: ', state, '\nnextState: ', nextState, '\ndiff: ', diff, '\n\n')
+    return next(_ => nextState)
+  })
+}
+
+export default store
